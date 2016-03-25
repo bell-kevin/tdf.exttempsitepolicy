680b224547431ec141991e27be53b1f581d87f11
andreasma <maand@gmx.de>
2016-03-25 17:50:56 +0100

    Adding a new frontpage with view for the LibreOffice extensions and templates website with a first draft of content (to be extended later).

---
+++ src/tdf/exttempsitepolicy/browser/frontpage.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+
+class frontpageView(BrowserView):
+
+    """ The view of the LibreOffice extensions and templates frontpage
+    """
+    index = ViewPageTemplateFile("templates/frontpageview.pt")
