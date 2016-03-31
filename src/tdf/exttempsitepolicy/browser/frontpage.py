15ed431c437e4431a0dd9d202dc2ab83d775835d
andreasma <maand@gmx.de>
2016-03-31 21:17:59 +0200

    Adding dynamic counting of extension and template projects to the front-page

--- src/tdf/exttempsitepolicy/browser/frontpage.py
+++ src/tdf/exttempsitepolicy/browser/frontpage.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from plone import api
 from Products.Five.browser import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from tdf.exttempsitepolicy import _
+from Acquisition import aq_inner
 
 
 
@@ -10,4 +10,46 @@ class frontpageView(BrowserView):
 
     """ The view of the LibreOffice extensions and templates frontpage
     """
+
+
+
+    def tupproject_count(self):
+        """Return number of projects
+        """
+        context = aq_inner(self.context)
+        catalog = api.portal.get_tool(name='portal_catalog')
+
+        return len(catalog(portal_type='tdf.templateuploadcenter.tupproject'))
+
+
+
+    def tuprelease_count(self):
+        """Return number of downloadable files
+        """
+        context = aq_inner(self.context)
+        catalog = api.portal.get_tool(name='portal_catalog')
+
+        return len(catalog(portal_type='tdf.templateuploadcenter.tuprelease'))
+
+
+
+
+    def eupproject_count(self):
+        """Return number of projects
+        """
+        context = aq_inner(self.context)
+        catalog = api.portal.get_tool(name='portal_catalog')
+
+        return len(catalog(portal_type='tdf.extensionuploadcenter.tupproject'))
+
+
+
+    def euprelease_count(self):
+        """Return number of downloadable files
+        """
+        context = aq_inner(self.context)
+        catalog = api.portal.get_tool(name='portal_catalog')
+
+        return len(catalog(portal_type='tdf.extensionuploadcenter.tuprelease'))
+
 
